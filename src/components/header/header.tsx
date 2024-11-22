539e4bbf5800f1a77d800903b3dd8b67ef7e1efd
Rosalinda190 <RosalindaGamez@MacBook-Pro-de-Rosy.local>
2024-11-22 00:11:39 -0600

    menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,20 @@
-import './header.css'
-import logoF1 from '../../assets/images/logoFI1.png'
-import UASLPLogo1 from '../../assets/images/UASLPLogo1.png'
-
+import './header.css';
+import logoF1 from '../../assets/images/logoFI1.png';
+import UASLPLogo1 from '../../assets/images/UASLPLogo1.png';
+import ModeButton from '../modeButton/modeButton'; // Importa el componente correctamente
 
 function Header() {
-
   return (
     <>
-        <header className='header'>
-            <img src={logoF1} className="logo" alt="logo" />
-            <img src={UASLPLogo1} className="logo" alt="logo" />
-        </header>
+      <header className='header'>
+        <img src={UASLPLogo1} className="logo" alt="logo" />
+        <img src={logoF1} className="logo" alt="logo" />
+        <div className="modeButton-container">
+        <ModeButton /> 
+        </div>
+      </header>
     </>
-
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import DoughnutChart from "../components/DoughnutChart/DoughnutChart";
 import Header from "../components/header/header";
 import Table from "../components/table/table";
+import Menu from "../components/Menu/menu";
 
 const headers = ['Header 1', 'Header 2', 'Header 3'];
 
@@ -14,33 +15,7 @@ function Dashboard() {
   return (
     <div>
       <Header />
-        <br />
-        <div style={
-            {
-                display: 'flex',
-                justifyContent: 'center',
-                width: '50%'
-            }
-        }>
-        </div>
-        <div
-            style={{
-                width: '1000px',
-                height: '1000px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-
-            }}
-        >
-            <DoughnutChart data={[
-                { name: 'A', value: 10, color: 'red' },
-                { name: 'B', value: 20, color: 'blue' },
-                { name: 'C', value: 30, color: 'green' },
-                { name: 'D', value: 40, color: 'orange' }
-            ]} />
-        </div>
-
+      <Menu />
 
     </div>
   );
